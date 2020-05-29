29656a3e03338a193f0f3769209ee57f5506598f
SafychCSS <safaraliev.renat@yandex.ru>
2020-05-29 18:03:07 +0300

    Updates

---
+++ precache-manifest.be3c3ae4aad97ab04b1381e9e6af50f9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e2f7941d61d00d2792c2edfef0531a",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "485d95f93a47486f24ec",
+    "url": "/test/static/css/3.9e963b7d.chunk.css"
+  },
+  {
+    "revision": "70f0716c52555b92865c",
+    "url": "/test/static/css/4.4765d6f0.chunk.css"
+  },
+  {
+    "revision": "1a3e065f8ccb1075b868",
+    "url": "/test/static/css/main.c01726c7.chunk.css"
+  },
+  {
+    "revision": "0acfffedb5951ebc1766",
+    "url": "/test/static/js/2.a7cab708.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test/static/js/2.a7cab708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485d95f93a47486f24ec",
+    "url": "/test/static/js/3.b0fbcef5.chunk.js"
+  },
+  {
+    "revision": "70f0716c52555b92865c",
+    "url": "/test/static/js/4.58b1e490.chunk.js"
+  },
+  {
+    "revision": "1a3e065f8ccb1075b868",
+    "url": "/test/static/js/main.0aa6cbfc.chunk.js"
+  },
+  {
+    "revision": "15dce585322710522804",
+    "url": "/test/static/js/runtime-main.226f24d2.js"
+  }
+]);
